5ca210c96145328199064266d8c63afd7e6a3871
RoxanneYang <yanzheyangchn@outlook.com>
2017-03-20 23:10:22 +0800

    Update uniitest for portfolio

--- pyStratAlpha/tests/analyzer/portfolio/testPortfolio.py
+++ pyStratAlpha/tests/analyzer/portfolio/testPortfolio.py
@@ -2,94 +2,130 @@
 import os
 import unittest
 from datetime import datetime
-
 import pandas as pd
-
+from pyStratAlpha.analyzer.factor.cleanData import get_multi_index_data
 from pyStratAlpha.analyzer.portfolio.portfolio import Portfolio
+from pyStratAlpha.enums import DataSource
 
 
 class TestPortfolio(unittest.TestCase):
     def setUp(self):
-        dirName = os.path.dirname(os.path.abspath(__file__))
-        path = os.path.join(dirName, 'data//sec_selected.csv')
-        factor = pd.read_csv(path)
-        factor.columns = ['tiaoCangDate', 'secID', 'score', 'weight', 'INDUSTRY']
+        dir_name = os.path.dirname(os.path.abspath(__file__))
+        input_path = os.path.join(dir_name, 'data//portfolio_input.csv')
+        result_path = os.path.join(dir_name, 'data//portfolio_result.csv')
+        filterd_path = os.path.join(dir_name, 'data//filtered_result.csv')
+        sec_data = pd.read_csv(input_path)
         index = pd.MultiIndex.from_arrays(
-            [[datetime.strptime(date, "%Y/%m/%d") for date in factor['tiaoCangDate'].values], factor['secID'].values],
+            [[datetime.strptime(date, "%Y-%m-%d") for date in sec_data['tiaoCangDate'].dropna().values],
+             sec_data['secID'].dropna().values],
             names=['tiaoCangDate', 'secID'])
-        secSelected = pd.DataFrame({'score': factor['score'].values,
-                                    'weight': factor['weight'].values,
-                                    'INDUSTRY': factor['INDUSTRY'].values}, index=index)
-        endDate = datetime.strptime('2016-2-29', "%Y-%m-%d")
-        self.portfolio = Portfolio(secSelected, endDate)
+        sec_selected = pd.DataFrame(
+            data={'score': sec_data['score'].dropna().values, 'weight': sec_data['weight'].dropna().values,
+                  'INDUSTRY': sec_data['INDUSTRY'].dropna().values}, index=index, dtype='float64')
+        sec_selected = sec_selected[['weight', 'INDUSTRY', 'score']]
+        self.portfolio = Portfolio(sec_selected=sec_selected, end_date='2012-11-30', data_source=DataSource.CSV,
+                                   csv_path=result_path)
+
+        secore_result = pd.read_csv(result_path)
+        self.score_result = pd.DataFrame(data=secore_result[range(1, len(secore_result.columns))].values,
+                                         index=secore_result['tradeDate'],
+                                         columns=secore_result[range(1, len(secore_result.columns))].columns)
+
+        self.filtered = pd.read_csv(filterd_path)
 
     def testGetSecPriceBetweenTiaoCangDate(self):
-        calculated = self.portfolio._get_sec_price_between_tiaocang_date(datetime(2016, 1, 29),
-                                                                         datetime(2016, 2, 1))
-        dirName = os.path.dirname(os.path.abspath(__file__))
-        path = os.path.join(dirName, 'data//result.csv')
-        expected = pd.read_csv(path)
-        expected = pd.DataFrame(data=expected.loc[:, list(expected.columns.values[1:])].values,
-                                index=[datetime.strptime(date, "%Y-%m-%d") for date in expected['tradeDate']],
-                                columns=expected.columns.values[1:])
-        expected.index.names = ['tradeDate']
+        calculated = self.portfolio._get_sec_price_between_tiaocang_date(datetime(2012, 6, 29),
+                                                                         datetime(2012, 7, 31))
+        expected = self.score_result
         pd.util.testing.assert_frame_equal(calculated, expected)
 
+    def testGetSecPriceOnDate(self):
+        calculated = self.portfolio._get_sec_price_on_date(self.score_result, '2012/7/4')
+        expected = self.score_result.loc['2012/7/4']
+        expected.name = 'price'
+        pd.util.testing.assert_series_equal(calculated, expected)
+
     def testGetWeightOnDate(self):
-        calculated = self.portfolio._get_weight_on_date(datetime(2016, 1, 29))
-        dirName = os.path.dirname(os.path.abspath(__file__))
-        path = os.path.join(dirName, 'data//expected.csv')
-        expected = pd.read_csv(path)
-        expected = pd.Series(data=expected['files'].values, dtype='int32', index=expected['secID'], name='files')
-        expected.index.names = ['secID']
-        pd.util.testing.assert_series_equal(calculated['files'], expected)
+        calculated = self.portfolio._get_weight_on_date(datetime(2012, 6, 29))
+
+        expected = pd.Series(data=self.filtered['weight2'].values,
+                             index=self.filtered['secID2'], name='weight')
+        expected.index.name = 'secID'
+        pd.util.testing.assert_series_equal(calculated, expected)
 
     def testFilterSecOnTiaoCangDate(self):
-        calculated = self.portfolio._filter_sec_on_tiaocang_date(datetime(2016, 1, 29),
-                                                                 ['000046.SZ', '000048.SZ', '601388.SH'])
-        expected = pd.Series(data=[0, 0, 1], index=['000046.SZ', '000048.SZ', '601388.SH'], dtype='int32',
-                             name='files')
-        expected.index.names = ['secID']
+        sec_id = ['000702.SZ', '600538.SH', '600975.SH', '002143.SZ', '002286.SZ', '002548.SZ', '002477.SZ',
+                  '002124.SZ', '600508.SH', '601001.SH', '600403.SH', '300164.SZ', '600971.SH', '300192.SZ',
+                  '600094.SH', '000985.SZ', '002217.SZ', '600378.SH', '300163.SZ', '002455.SZ', '002054.SZ',
+                  '002343.SZ', '002113.SZ', '600731.SH', '600985.SH', '000782.SZ', '002361.SZ', '300132.SZ',
+                  '300041.SZ', '000953.SZ', '002201.SZ', '300109.SZ', '600527.SH', '002395.SZ', '300019.SZ',
+                  '002165.SZ', '002274.SZ', '600784.SH', '000825.SZ', '600399.SH', '002075.SZ', '600019.SH',
+                  '002295.SZ', '601137.SH', '600888.SH', '002057.SZ', '600338.SH', '600459.SH', '600219.SH',
+                  '002116.SZ', '002082.SZ', '002066.SZ', '002088.SZ', '600562.SH', '600552.SH', '000856.SZ',
+                  '600248.SH', '600970.SH', '002062.SZ', '600881.SH', '300040.SZ', '600984.SH', '600847.SH',
+                  '600992.SH', '002175.SZ', '600560.SH', '600558.SH', '000967.SZ', '000862.SZ', '000595.SZ',
+                  '002031.SZ', '300126.SZ', '002347.SZ', '002150.SZ', '002510.SZ', '300066.SZ', '002158.SZ',
+                  '002026.SZ', '300173.SZ', '300018.SZ', '300069.SZ', '600566.SH', '600582.SH', '002090.SZ',
+                  '000676.SZ', '002209.SZ', '300099.SZ', '002559.SZ', '601717.SH', '300124.SZ', '002196.SZ',
+                  '002204.SZ', '600973.SH', '002214.SZ', '002045.SZ', '300083.SZ', '002139.SZ', '300046.SZ',
+                  '002076.SZ', '600237.SH', '000413.SZ', '002402.SZ', '002484.SZ', '300076.SZ', '002414.SZ',
+                  '000700.SZ', '002101.SZ', '600148.SH', '002488.SZ', '600081.SH', '002126.SZ', '002472.SZ',
+                  '002213.SZ', '600990.SH', '600715.SH', '300008.SZ', '000957.SZ', '300038.SZ', '002152.SZ',
+                  '000748.SZ', '000561.SZ', '002197.SZ', '000070.SZ', '000555.SZ', '000851.SZ', '002544.SZ',
+                  '002035.SZ', '000921.SZ', '600336.SH', '600854.SH', '000801.SZ', '002495.SZ', '000796.SZ',
+                  '000729.SZ', '002557.SZ', '000869.SZ', '002044.SZ', '600987.SH', '002404.SZ', '600689.SH',
+                  '002098.SZ', '002425.SZ', '600626.SH', '002117.SZ', '002235.SZ', '000910.SZ', '600356.SH',
+                  '600966.SH', '300089.SZ', '002247.SZ', '600210.SH', '600385.SH', '000603.SZ', '600671.SH',
+                  '002252.SZ', '300016.SZ', '000756.SZ', '000963.SZ', '600222.SH', '002275.SZ', '600706.SH',
+                  '600530.SH', '000605.SZ', '300015.SZ', '600513.SH', '002004.SZ', '600796.SH', '000411.SZ',
+                  '600594.SH', '000692.SZ', '300070.SZ', '000695.SZ', '600982.SH', '600758.SH', '600461.SH',
+                  '600167.SH', '600780.SH', '600561.SH', '002040.SZ', '000582.SZ', '002357.SZ', '600077.SH',
+                  '002245.SZ', '002210.SZ', '300013.SZ', '600734.SH', '600463.SH', '000909.SZ', '000502.SZ',
+                  '600791.SH', '000628.SZ', '000546.SZ', '600647.SH', '000732.SZ', '600684.SH', '600340.SH',
+                  '600823.SH', '002133.SZ', '600136.SH', '002500.SZ', '601009.SH', '600816.SH', '601818.SH',
+                  '601169.SH', '600856.SH', '600738.SH', '000419.SZ', '600735.SH', '600278.SH', '600861.SH',
+                  '600694.SH', '002024.SZ', '600729.SH', '000610.SZ', '000430.SZ', '000524.SZ', '000007.SZ',
+                  '601007.SH', '000892.SZ', '600571.SH', '600757.SH', '300074.SZ', '600446.SH', '600718.SH',
+                  '300058.SZ', '000682.SZ', '300047.SZ', '002253.SZ', '600570.SH', '600588.SH', '000504.SZ',
+                  '600892.SH', '000409.SZ', '600656.SH', '000710.SZ', '600701.SH', '000556.SZ', '000832.SZ',
+                  '000660.SZ', '600670.SH', '000689.SZ']
+
+        calculated = self.portfolio._filter_sec_on_tiaocang_date(datetime(2012, 6, 29), sec_id)
+
+        expected = pd.Series(data=self.filtered['filtered'].dropna().values,
+                             index=self.filtered['sec_id'].dropna(), dtype='int32', name='filters')
         pd.util.testing.assert_series_equal(calculated, expected)
 
     def testUpdateWeightAfterFilter(self):
-        weight = get_multi_index_data(self.portfolio._secSelected, 'tiaoCangDate', datetime(2016, 1, 29), 'secID',
-                                      ['000046.SZ', '000048.SZ', '000062.SZ'])
+        filter = pd.Series(data=self.filtered['filtered'].dropna().values,
+                           index=self.filtered['sec_id'].dropna(), dtype='int32', name='filters')
+        weight = get_multi_index_data(self.portfolio._sec_selected, 'tiaoCangDate', datetime(2012, 6, 29), 'secID',
+                                      filter.index.values)
         weight = weight.reset_index().set_index('secID')
-        weight = weight.drop(['tiaoCangDate'], axis=1)
-        files = self.portfolio._filter_sec_on_tiaocang_date(datetime(2015, 10, 27), weight.index.tolist())
-        calculated = self.portfolio._update_weight_after_filter(weight, files)
-        expected = pd.Series(data=[0.005095556, 0.0, 0.005096429], index=['000062.SZ', '000048.SZ', '000046.SZ'],
-                             name='weight')
-        expected.index.names = ['secID']
-        pd.util.testing.assert_series_equal(calculated['weight'], expected)
+        weight = weight.drop(['tiaoCangDate', 'score'], axis=1)
+        weight = weight[['weight', 'INDUSTRY']]
+        calculated = self.portfolio._update_weight_after_filter(weight, filter)
+
+        expected = pd.DataFrame(data=self.filtered[['weight', 'INDUSTRY', 'filters']].values,
+                                index=self.filtered['secID'], columns=['weight', 'INDUSTRY', 'filters'])
+        expected['weight'] = expected['weight'].astype('float64')
+        expected['filters'] = expected['filters'].astype('int32')
+        pd.util.testing.assert_frame_equal(calculated, expected)
 
     def testGetQuantity(self):
-        initPtfValue = 10000000
-        tiaoCangStartDate = datetime(2016, 1, 29)
-        tiaoCangEndDate = datetime(2016, 2, 1)
-        priceData = self.portfolio._get_sec_price_between_tiaocang_date(tiaoCangStartDate, tiaoCangEndDate)
-        price = self.portfolio._get_sec_price_on_date(priceData, tiaoCangStartDate)
-        weight = self.portfolio._get_weight_on_date(tiaoCangStartDate)
-        calculated = self.portfolio._get_quantity(initPtfValue, weight, price)
-        price = pd.Series(data=[23.79, 8.38, 27.95, 56.9, 9.83325306, 37.13,
-                                23.3077714, 14.09744261, 6.34956119, 6.26],
-                          index=pd.Index(['000007.SZ', '000008.SZ', '000034.SZ', '000038.SZ', '000046.SZ',
-                                          '000048.SZ', '000062.SZ', '000063.SZ', '000069.SZ', '000078.SZ'],
-                                         dtype='object'), name='price')
-        weight = pd.Series(data=[[0., 0.00201654, 0., 0., 0.00509643, 0.00398625,
-                                  0.00509556, 0.00413833, 0.00509643, 0.0060155]],
-                           index=pd.Index(['000007.SZ', '000008.SZ', '000034.SZ', '000038.SZ', '000046.SZ',
-                                           '000048.SZ', '000062.SZ', '000063.SZ', '000069.SZ', '000078.SZ'],
-                                          dtype='object', name='secID'), name='weight')
+        init_ptf_value = 10000000
+        filter = pd.Series(data=self.filtered['filtered'].dropna().values,
+                           index=self.filtered['sec_id'].dropna(), dtype='int32', name='filters')
+        weight = get_multi_index_data(self.portfolio._sec_selected, 'tiaoCangDate', datetime(2012, 6, 29), 'secID',
+                                      filter.index.values)
+        weight = weight.reset_index().set_index('secID')
+        weight = weight.drop(['tiaoCangDate', 'score'], axis=1)
+        weight = weight[['weight', 'INDUSTRY']]
+        price = self.score_result.loc['2012/7/4']
+        price.name = 'price'
+        calculated = self.portfolio._get_quantity(init_ptf_value, weight, price)
 
-        pass
-
-    def testCalcPtfValueBetweenTiaoCangDate(self):
-        pass
-
-    def testCalcPtfValueCurve(self):
-        pass
-
-    def testEvaluatePtfReturn(self):
-        pass
+        expected = pd.Series(data=self.filtered['quantity'].values,
+                             index=self.filtered['secID3'], name='quantity')
+        expected.index.name = 'secID'
+        pd.util.testing.assert_series_equal(calculated, expected)
