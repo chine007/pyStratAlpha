36cf9dbea0c1ae126f79d5fe6396ba9db0e5cae1
iLampard <siqiao_xue@163.com>
2017-03-26 00:05:07 +0800

    clean improperly written code in unit test

--- pyStratAlpha/tests/analyzer/performance/testNavAnalyzer.py
+++ pyStratAlpha/tests/analyzer/performance/testNavAnalyzer.py
@@ -3,6 +3,8 @@ import os
 import unittest
 from datetime import datetime
 import pandas as pd
+from pandas.util.testing import assert_series_equal
+from pandas.util.testing import assert_frame_equal
 from pyStratAlpha.analyzer.performance.navAnalyzer import print_perf_stat_by_year
 from pyStratAlpha.analyzer.performance.navAnalyzer import ptf_re_balance
 from pyStratAlpha.analyzer.performance.navAnalyzer import regroup_by_re_balance_freq
@@ -13,23 +15,25 @@ from pyStratAlpha.enums import ReturnType
 class TestNavAnalyzer(unittest.TestCase):
     def setUp(self):
         dir_name = os.path.dirname(os.path.abspath(__file__))
-        readfile = pd.read_csv(dir_name + "//data//performance.csv")
-        readfile.columns = ['tradeDate', 'benchmark', 'strategy']
-        strategy = readfile['strategy'].values
-        benchmark = readfile['benchmark'].values
-        index = [datetime.strptime(date, '%Y/%m/%d') for date in readfile['tradeDate']]
-        self.strategy_data_frame = pd.DataFrame(data=strategy, index=index)
-        self.strategy_series = pd.Series(data=strategy, index=index)
-        self.benchmark_data_frame = pd.DataFrame(data=benchmark, index=index)
-        self.returnDict = {'stratReturn': [self.strategy_data_frame, ReturnType.Cumul],
-                           'benchmarkReturn': [self.benchmark_data_frame, ReturnType.Cumul]}
-        result_file = pd.read_csv(dir_name + "//data//result.csv")
-        result_file.columns = ['tradeDate', 'result1', 'result2', 'result3']
-        self.index = [datetime.strptime(date, '%Y/%m/%d') for date in result_file['tradeDate']]
-        self.result = [result_file['result1'].values, result_file['result2'].values, result_file['result3'].values]
+        return_data = pd.read_csv(dir_name + '//data//performance.csv')
+        return_data.columns = ['tradeDate', 'benchmark', 'strategy']
+        return_data['tradeDate'] = pd.to_datetime(return_data['tradeDate'], format='%Y/%m/%d')
+        return_data.set_index('tradeDate', inplace=True)
+
+        self.strategy = return_data['strategy']
+        self.benchmark = return_data['benchmark']
+
+        self.returnDict = {'stratReturn': [self.strategy, ReturnType.Cumul],
+                           'benchmarkReturn': [self.benchmark, ReturnType.Cumul]}
+
+        result_data = pd.read_csv(dir_name + "//data//result.csv")
+        result_data.columns = ['tradeDate', 'result1', 'result2', 'result3']
+        result_data['tradeDate'] = pd.to_datetime(result_data['tradeDate'], format='%Y/%m/%d')
+        result_data.set_index('tradeDate', inplace=True)
+        self.result = result_data
 
     def testRegroupByBalanceFreq(self):
-        group = regroup_by_re_balance_freq(self.strategy_data_frame)
+        group = regroup_by_re_balance_freq(self.strategy)
         calculated = {}
         for (name, value) in group.groups.items():
             calculated[datetime.strptime(str(name), "%Y-%m-%d %H:%M:%S")] = value
@@ -41,62 +45,54 @@ class TestNavAnalyzer(unittest.TestCase):
                     datetime(2005, 9, 30, 0, 0): 182, datetime(2005, 5, 31, 0, 0): 94}
         self.assertEqual(calculated, expected)
 
-        group = regroup_by_re_balance_freq(self.strategy_series)
-        calculated = {}
-        for (name, value) in group.groups.items():
-            calculated[datetime.strptime(str(name), "%Y-%m-%d %H:%M:%S")] = value
-        self.assertEqual(calculated, expected)
-
-        group_val = regroup_by_re_balance_freq(self.strategy_data_frame, FreqType.EOM)
+        group_val = regroup_by_re_balance_freq(self.strategy, FreqType.EOM)
         calculated = {}
         for (name, value) in group_val.groups.items():
             calculated[datetime.strptime(str(name), "%Y-%m-%d %H:%M:%S")] = value
+        expected = {datetime(2005, 2, 28, 0, 0): 33, datetime(2005, 8, 31, 0, 0): 160,
+                    datetime(2005, 6, 30, 0, 0): 116, datetime(2005, 4, 30, 0, 0): 77,
+                    datetime(2005, 12, 31, 0, 0): 242, datetime(2005, 3, 31, 0, 0): 56,
+                    datetime(2005, 11, 30, 0, 0): 220, datetime(2005, 7, 31, 0, 0): 137,
+                    datetime(2005, 10, 31, 0, 0): 198, datetime(2005, 1, 31, 0, 0): 20,
+                    datetime(2005, 9, 30, 0, 0): 182, datetime(2005, 5, 31, 0, 0): 94}
         self.assertEqual(calculated, expected)
 
-        group_val = regroup_by_re_balance_freq(self.strategy_series, FreqType.EOM)
-        calculated = {}
-        for (name, value) in group_val.groups.items():
-            calculated[datetime.strptime(str(name), "%Y-%m-%d %H:%M:%S")] = value
-        self.assertEqual(calculated, expected)
-
-        group_val = regroup_by_re_balance_freq(self.strategy_data_frame, FreqType.EOY)
+        group_val = regroup_by_re_balance_freq(self.strategy, FreqType.EOY)
         calculated = {}
         for (name, value) in group_val.groups.items():
             calculated[datetime.strptime(str(name), "%Y-%m-%d %H:%M:%S")] = value
         expected = {datetime(2005, 12, 31, 0, 0): 242}
         self.assertEqual(calculated, expected)
 
-        group_val = regroup_by_re_balance_freq(self.strategy_series, FreqType.EOY)
-        calculated = {}
-        for (name, value) in group_val.groups.items():
-            calculated[datetime.strptime(str(name), "%Y-%m-%d %H:%M:%S")] = value
-        self.assertEqual(calculated, expected)
-
     def testPtfRebalance(self):
-        return_dict = {'stratReturn': [self.strategy_data_frame, ReturnType.Cumul],
-                       'benchmarkReturn': [self.benchmark_data_frame, ReturnType.Cumul]}
+        return_dict = {'stratReturn': [self.strategy, ReturnType.Cumul],
+                       'benchmarkReturn': [self.benchmark, ReturnType.Cumul]}
         calculated = ptf_re_balance(return_dict)
-        expected = pd.Series(data=self.result[0], index=self.index, name='hedgedPtfReturn')
-        pd.util.testing.assert_series_equal(calculated, expected)
+        expected = self.result['result1']
+        expected.name = 'hedgedPtfReturn'
+        assert_series_equal(calculated, expected)
 
         calculated = ptf_re_balance(return_dict, margin_prop=0.5)
-        expected = pd.Series(data=self.result[1], index=self.index, name='hedgedPtfReturn')
-        pd.util.testing.assert_series_equal(calculated, expected)
+        expected = self.result['result2']
+        expected.name = 'hedgedPtfReturn'
+        assert_series_equal(calculated, expected)
 
         calculated = ptf_re_balance(return_dict, margin_prop=0.5, re_balance_freq=FreqType.EOY)
-        expected = pd.Series(data=self.result[2], index=self.index, name='hedgedPtfReturn')
-        pd.util.testing.assert_series_equal(calculated, expected)
+        expected = self.result['result3']
+        expected.name = 'hedgedPtfReturn'
+        assert_series_equal(calculated, expected)
 
     def testPrintPerfStatYear(self):
-        calculated = print_perf_stat_by_year(self.strategy_series, ReturnType.Cumul)
+        #TODO fix this
+        calculated = print_perf_stat_by_year(self.strategy, ReturnType.Cumul)
         expected = pd.DataFrame(data=[-0.101456, -0.295517, -0.343317, -0.289605],
                                 index=['annual_return', 'calmar_ratio', 'max_drawdown', 'sharpe_ratio'], columns=[2005])
-        pd.util.testing.assert_frame_equal(calculated, expected)
+        assert_frame_equal(calculated, expected)
 
-        calculated = print_perf_stat_by_year(self.strategy_series, ReturnType.NonCumul)
+        calculated = print_perf_stat_by_year(self.strategy, ReturnType.NonCumul)
         expected = pd.DataFrame(columns=pd.Int64Index([], dtype='int64'),
                                 index=['annual_return', 'calmar_ratio', 'max_drawdown', 'sharpe_ratio'])
-        pd.util.testing.assert_frame_equal(calculated, expected)
+        assert_frame_equal(calculated, expected)
 
         # def testPerfStat(self):
         #     calculatedStat, calculatedStatSign = perf_stat(self.strategy_series, self.benchmark_data_frame)
