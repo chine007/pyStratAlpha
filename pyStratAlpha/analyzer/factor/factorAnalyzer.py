b3993414e66e17f7c6f302c78536c48a5831e41c
jihui <jihui0714@126.com>
2017-03-30 14:05:49 +0800

    corret bug of date

--- pyStratAlpha/analyzer/factor/factorAnalyzer.py
+++ pyStratAlpha/analyzer/factor/factorAnalyzer.py
@@ -13,6 +13,7 @@ from alphalens.utils import get_clean_factor_and_forward_returns
 from alphalens.plotting import plot_mean_quantile_returns_spread_time_series
 from alphalens.plotting import plot_cumulative_returns_by_quantile
 from alphalens.tears import GridFigure
+from alphalens.plotting import plot_quantile_returns_bar
 from pyStratAlpha.analyzer.indexComp import IndexComp
 from pyStratAlpha.enums import DataSource
 from pyStratAlpha.utils import get_sec_price
@@ -52,6 +53,7 @@ class FactorAnalyzer(object):
         self._factor.index = self._factor.index.rename(_alphaLensFactorIndexName)
         self._factor.name = _alphaLensFactorColName
 
+
         self._trade_date = sorted(set(self._factor.index.get_level_values(_alphaLensFactorIndexName[0])))
         self._sec_ID = sorted(set(self._factor.index.get_level_values(_alphaLensFactorIndexName[1])))
 
@@ -86,11 +88,20 @@ class FactorAnalyzer(object):
                                                                           std_err=std_err)
         plot_mean_quantile_returns_spread_time_series(quant_return_spread, std_err_spread)
 
+    @classmethod
+    def mean_return_tear_sheet(cls,factor):
+        mean_return_by_q, std_err_by_q = mean_return_by_quantile(factor, by_group=False)
+        plot_quantile_returns_bar(mean_return_by_q)
+
+
+
     def create_full_tear_sheet(self):
         factor_data = self._get_clean_factor_and_fwd_return()
+
         for p in self._periods:
             self.quantile_tear_sheet(factor_data, p)
         self.top_bottom_tear_sheet(factor_data)
+        self.mean_return_tear_sheet(factor_data)
         plt.show()
         return
 
@@ -101,7 +112,7 @@ def sec_score_convert2daily(sec_score, start_date, end_date, calendar='China.SSE
     :param start_date: str, start date of sec_score to be used
     :param end_date: str, end date of sec_score to be used
     :param calendar: str, calendar defined in PyFin
-    :return:
+    :return: ret: dataframe, col = [tiaoCangDate, secID, score/factor] frequency of tiaoCangDate is Day
     """
     sec_score = sec_score.loc[sec_score['tiaoCangDate'] >= start_date, :]
     sec_score = sec_score.loc[sec_score['tiaoCangDate'] <= end_date, :]
@@ -112,6 +123,7 @@ def sec_score_convert2daily(sec_score, start_date, end_date, calendar='China.SSE
     ret = pd.DataFrame()
     for i in range(0, len(tiaocang_date_list) - 1):
         date = bizDatesList(calendar, tiaocang_date_list[i], tiaocang_date_list[i + 1])
+        date = date if i == len(tiaocang_date_list)-2 else date[:-1]
         nb_date = len(date)
         tiancang_sec_score = sec_score.loc[sec_score['tiaoCangDate'] >= tiaocang_date_list[i], :]
         tiancang_sec_score = tiancang_sec_score.loc[tiancang_sec_score['tiaoCangDate'] < tiaocang_date_list[i + 1], :]
@@ -128,6 +140,6 @@ def sec_score_convert2daily(sec_score, start_date, end_date, calendar='China.SSE
 
 if __name__ == "__main__":
     sec_score_raw = pd.read_csv(_sec_score_path, encoding='gbk')
-    analyzer = FactorAnalyzer(start_date='2016-06-01', end_date='2016-08-01', factor_raw=sec_score_raw,
-                              data_source=DataSource.WIND)
+    analyzer = FactorAnalyzer(start_date='2011-01-01', end_date='2013-01-01', factor_raw=sec_score_raw,
+                              data_source=DataSource.MYSQL_LOCAL)
     analyzer.create_full_tear_sheet()
